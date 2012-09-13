00d44d2d481305939bbe8a6d1069ec710d03528e
Dean <dean.lozo@gmail.com>
2012-09-13 20:53:29 +0200

    original application
    The original application with an unpolished interface, and no only the
    shutdown feature.

---
+++ misc-timer/Window_Main.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Timed_Shutdown
+{
+    partial class Window_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerHours = new System.Windows.Forms.NumericUpDown();
+            this.timerSeconds = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerMinutes = new System.Windows.Forms.NumericUpDown();
+            this.labelHours = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.labelMinutes = new System.Windows.Forms.Label();
+            this.comboTask = new System.Windows.Forms.ComboBox();
+            this.checkBoxForced = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.timerHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerSeconds)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timerMinutes)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(61, 129);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 25);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerHours
+            // 
+            this.timerHours.Location = new System.Drawing.Point(15, 3);
+            this.timerHours.Name = "timerHours";
+            this.timerHours.Size = new System.Drawing.Size(41, 20);
+            this.timerHours.TabIndex = 6;
+            this.timerHours.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // timerSeconds
+            // 
+            this.timerSeconds.Location = new System.Drawing.Point(129, 3);
+            this.timerSeconds.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.timerSeconds.Name = "timerSeconds";
+            this.timerSeconds.Size = new System.Drawing.Size(41, 20);
+            this.timerSeconds.TabIndex = 3;
+            this.timerSeconds.ValueChanged += new System.EventHandler(this.timerSeconds_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.timerSeconds);
+            this.panel1.Controls.Add(this.timerHours);
+            this.panel1.Controls.Add(this.timerMinutes);
+            this.panel1.Location = new System.Drawing.Point(7, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(190, 46);
+            this.panel1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 14);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Seconds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Minutes";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 14);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hours";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timerMinutes
+            // 
+            this.timerMinutes.Location = new System.Drawing.Point(71, 3);
+            this.timerMinutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.timerMinutes.Name = "timerMinutes";
+            this.timerMinutes.Size = new System.Drawing.Size(41, 20);
+            this.timerMinutes.TabIndex = 5;
+            // 
+            // labelHours
+            // 
+            this.labelHours.AutoSize = true;
+            this.labelHours.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHours.Location = new System.Drawing.Point(32, 108);
+            this.labelHours.Name = "labelHours";
+            this.labelHours.Size = new System.Drawing.Size(26, 18);
+            this.labelHours.TabIndex = 8;
+            this.labelHours.Text = "00";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(61, 129);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 25);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelSeconds
+            // 
+            this.labelSeconds.AutoSize = true;
+            this.labelSeconds.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSeconds.Location = new System.Drawing.Point(151, 108);
+            this.labelSeconds.Name = "labelSeconds";
+            this.labelSeconds.Size = new System.Drawing.Size(26, 18);
+            this.labelSeconds.TabIndex = 4;
+            this.labelSeconds.Text = "00";
+            this.labelSeconds.Click += new System.EventHandler(this.labelSeconds_Click);
+            // 
+            // labelMinutes
+            // 
+            this.labelMinutes.AutoSize = true;
+            this.labelMinutes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMinutes.Location = new System.Drawing.Point(93, 108);
+            this.labelMinutes.Name = "labelMinutes";
+            this.labelMinutes.Size = new System.Drawing.Size(26, 18);
+            this.labelMinutes.TabIndex = 7;
+            this.labelMinutes.Text = "00";
+            this.labelMinutes.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboTask
+            // 
+            this.comboTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboTask.FormattingEnabled = true;
+            this.comboTask.Items.AddRange(new object[] {
+            "Log Off",
+            "Shutdown",
+            "Reboot"});
+            this.comboTask.Location = new System.Drawing.Point(71, 11);
+            this.comboTask.Name = "comboTask";
+            this.comboTask.Size = new System.Drawing.Size(115, 21);
+            this.comboTask.TabIndex = 9;
+            this.comboTask.SelectedIndexChanged += new System.EventHandler(this.comboTask_SelectedIndexChanged);
+            // 
+            // checkBoxForced
+            // 
+            this.checkBoxForced.AutoSize = true;
+            this.checkBoxForced.Location = new System.Drawing.Point(5, 14);
+            this.checkBoxForced.Name = "checkBoxForced";
+            this.checkBoxForced.Size = new System.Drawing.Size(60, 18);
+            this.checkBoxForced.TabIndex = 10;
+            this.checkBoxForced.Text = "Forced";
+            this.checkBoxForced.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBoxForced);
+            this.panel2.Controls.Add(this.comboTask);
+            this.panel2.Location = new System.Drawing.Point(7, 55);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(190, 50);
+            this.panel2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 161);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.labelHours);
+            this.Controls.Add(this.labelMinutes);
+            this.Controls.Add(this.labelSeconds);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "STimer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timerHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timerSeconds)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timerMinutes)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown timerHours;
+        private System.Windows.Forms.NumericUpDown timerSeconds;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown timerMinutes;
+        private System.Windows.Forms.Label labelHours;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelSeconds;
+        private System.Windows.Forms.Label labelMinutes;
+        private System.Windows.Forms.ComboBox comboTask;
+        private System.Windows.Forms.CheckBox checkBoxForced;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ misc-timer/Window_Main.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace Timed_Shutdown
+{
+    public partial class Window_Main : Form
+    {
+        public decimal seconds { get; set; }
+        public decimal minutes { get; set; }
+        public decimal hours { get; set; }
+        public Boolean paused { get; set; }
+        public int flag { get; set; }
+
+        public Window_Main()
+        {
+            InitializeComponent();
+            timer1.Interval = (1000) * (1);
+            timer1.Enabled = false;
+            buttonStop.Visible = false;
+        }
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (comboTask.SelectedIndex != -1)
+            {
+                //disable number boxes
+                timerSeconds.Enabled = false;
+                timerMinutes.Enabled = false;
+                timerHours.Enabled = false;
+                checkBoxForced.Enabled = false;
+                comboTask.Enabled = false;
+                //
+
+                seconds = timerSeconds.Value;
+                minutes = timerMinutes.Value;
+                hours = timerHours.Value;
+
+                updateLabels();
+
+                timer1.Enabled = true;
+                buttonStart.Visible = false;
+                buttonStop.Visible = true;
+                if (checkBoxForced.Checked)
+                {
+                    flag = comboTask.SelectedIndex + 4;
+                }
+                else
+                {
+                    flag = comboTask.SelectedIndex;
+                }
+            }
+            else
+            {
+                MessageBox.Show("You need to select an action!");
+            }
+
+            //MessageBox.Show(flag.ToString());
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            seconds = 0;
+            minutes = 0;
+            hours = 0;
+
+            timerSeconds.Enabled = true;
+            timerMinutes.Enabled = true;
+            timerHours.Enabled = true;
+            checkBoxForced.Enabled = true;
+            comboTask.Enabled = true;
+
+            updateLabels();
+
+            buttonStop.Visible = false;
+            buttonStart.Visible = true;
+
+            
+        }
+    
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (seconds == 0 && minutes == 0 && hours == 0)
+            {
+                timer1.Enabled = false;
+                ShutDownComputer(flag);
+                MessageBox.Show("Shutdown!");
+            }
+            else
+            {
+                if (seconds < 1)
+                {
+                    seconds = 59;
+                    if (minutes == 0)
+                    {
+                        minutes = 59;
+                        if (hours != 0)
+                        {
+                            hours--;
+                        }
+                    }
+                    else
+                    {
+                        minutes--;
+                    }
+
+                }
+                else 
+                {
+                    seconds--;
+                }
+            }
+            updateLabels();
+        }
+
+        private void timerSeconds_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void ShutDownComputer(int flag)
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+            // You can't shutdown without security privileges
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams = mcWin32.GetMethodParameters("Win32Shutdown");
+            // Flag 1 means we want to shut down the system
+            mboShutdownParams["Flags"] = flag;
+            mboShutdownParams["Reserved"] = "0";
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
+            }
+        }
+        private void updateLabels()
+        {
+            labelSeconds.Text = seconds.ToString();
+            labelMinutes.Text = minutes.ToString();
+            labelHours.Text = hours.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelSeconds_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboTask_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
